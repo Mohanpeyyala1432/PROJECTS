b27674e190c61a8fba95f480e0b83feeb5c7da42
mohan peyyala <mohanpeyyala2000@gmail.com>
2025-05-22 09:47:23 +0530

    my project code

---
+++ Backend/src/main/java/com/ihub/www/model/Department.java
@@ -0,0 +1,27 @@
+package com.ihub.www.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.transaction.Transactional;
+import lombok.Data;
+
+@Entity
+@Table
+@Data
+@Transactional
+public class Department
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long deptId;
+	
+	@Column
+	private String deptName;
+	
+	@Column
+	private String designation;
+}
